ff9fb6a995507c710d2ea4ffe629fcf901388da5
huntereureka <53546873+huntereureka@users.noreply.github.com>
2021-02-09 16:16:34 +0800

    Pull req 1 (#9)
    * test
    
    not sure if this is the right way to do this
    
    * extra file
    
    Co-authored-by: Hui Feng <42214211+TLI-Tristan@users.noreply.github.com>

---
+++ Hospital-Management-Software/feedback.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EDP_Presentation
+{
+    public partial class feedback : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_click(object sender, EventArgs e)
+        {
+            if ((string.IsNullOrEmpty(tbFeedback.Text)) | (string.IsNullOrEmpty(tbEmail.Text)) | (string.IsNullOrEmpty(tbSubject.Text)))
+            {
+                errorMsg.Text = "Please fill in required fields!";
+                errorMsg.ForeColor = System.Drawing.Color.Red;
+                errorMsg.Visible = true;
+                errorSubj.Text = "This is a required field.";
+                errorSubj.ForeColor = System.Drawing.Color.Red;
+                errorSubj.Visible = true;
+                errorFeedback.Text = "This is a required field.";
+                errorFeedback.ForeColor = System.Drawing.Color.Red;
+                errorFeedback.Visible = true;
+                errorEmail.Text = "This is a required field.";
+                errorEmail.ForeColor = System.Drawing.Color.Red;
+                errorEmail.Visible = true;
+            } 
+            else
+            {
+                ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+                int result = client.CreateFeedback(tbName.Text, tbEmail.Text, tbSubject.Text, tbFeedback.Text);
+                Response.Redirect("success.aspx");
+            }
+        }
+    }
+}
