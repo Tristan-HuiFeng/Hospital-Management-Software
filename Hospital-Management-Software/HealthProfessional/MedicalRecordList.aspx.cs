f43025e7bb6682bc45251307cddae9028ed195b8
Chew Hui Feng <tristan.chf@gmail.com>
2021-02-15 10:42:04 +0800

    Final update
    - Fix creation and view medical record data retrieval
    - simple change of html

--- Hospital-Management-Software/HealthProfessional/MedicalRecordList.aspx.cs
+++ Hospital-Management-Software/HealthProfessional/MedicalRecordList.aspx.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Hospital_Management_Software.HealthProfessional
 {
@@ -57,7 +59,10 @@ namespace Hospital_Management_Software.HealthProfessional
         {
             MyDBServiceReference.Service1Client client = new MyDBServiceReference.Service1Client();
 
-            DataTable dt = client.GetMedicalRecordTableView();
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+            int doctor_id = client.GetEmpIDByAccID(userManager.FindByName(System.Web.HttpContext.Current.User.Identity.Name).Id);
+            DataTable dt = client.GetMedicalRecordTableView(doctor_id);
 
             gv_MedicalRecordList.DataSource = dt;
             gv_MedicalRecordList.DataBind();
@@ -78,8 +83,12 @@ namespace Hospital_Management_Software.HealthProfessional
         protected void gv_MedicalRecordList_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
             MyDBServiceReference.Service1Client client = new MyDBServiceReference.Service1Client();
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
 
-            gv_MedicalRecordList.DataSource = client.GetMedicalRecordTableView();
+            int doctor_id = client.GetEmpIDByAccID(userManager.FindByName(System.Web.HttpContext.Current.User.Identity.Name).Id);
+
+            gv_MedicalRecordList.DataSource = client.GetMedicalRecordTableView(doctor_id);
             gv_MedicalRecordList.PageIndex = e.NewPageIndex;
             gv_MedicalRecordList.DataBind();
         }
